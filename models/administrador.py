a7707670666daff331e8739f369068f50aec6ed8
Eduardo Faleiro <eduardoviniciusfaleiro@gmail.com>
2025-09-15 19:17:16 -0300

    feat(models): Alterados models para ter getters e setters e seguir diagrama

--- models/administrador.py
+++ models/administrador.py
@@ -1,9 +1,28 @@
 from models.pessoa import Pessoa
-
+from models.porto import Porto
 
 class Administrador(Pessoa):
-    def __init__(self, nome: str, telefone: str):
+    def __init__(self, nome: str, telefone: str, porto: Porto):
         super().__init__(nome)
         self.__telefone = telefone
+        self.__porto = porto
 
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self, telefone: str):
+        if not isinstance(telefone, str):
+            raise TypeError("O telefone deve ser uma string.")
+        self.__telefone = telefone
     
+    @property
+    def porto(self):
+        return self.__porto
+
+    @porto.setter
+    def porto(self, porto: Porto):
+        if not isinstance(porto, Porto):
+            raise TypeError("O porto deve ser uma instância da classe Porto.")
+        self.__porto = porto
--- models/capitao.py
+++ models/capitao.py
@@ -1,11 +1,29 @@
 from models.pessoa import Pessoa
 from models.companhia import Companhia
+from models.navio import Navio
 
 class Capitao(Pessoa):
-    def __init__(self, nome: str, companhia: Companhia):
+    def __init__(self, nome: str, companhia: Companhia, navio: Navio):
         super().__init__(nome)
         self.__companhia = companhia
+        self.__navio = navio
 
     @property
     def companhia(self):
         return self.__companhia
+    
+    @companhia.setter
+    def companhia(self, companhia: Companhia):
+        if not isinstance(companhia, Companhia):
+            raise TypeError("A companhia deve ser uma instância da classe Companhia.")
+        self.__companhia = companhia
+
+    @property
+    def navio(self):
+        return self.__navio
+    
+    @navio.setter
+    def navio(self, navio: Navio):
+        if not isinstance(navio, Navio):
+            raise TypeError("O navio deve ser uma instância da classe Navio.")
+        self.__navio = navio
