cfaf30011189160de88ce542057ecce7cb5f43dc
Eduardo Faleiro <eduardoviniciusfaleiro@gmail.com>
2025-10-03 00:59:54 -0300

    Adicionado campo de código para funcionar com códigos do tipo ISO 3166

--- models/pais.py
+++ models/pais.py
@@ -1,13 +1,20 @@
 class Pais:
-    def __init__(self, nome: str):
+    def __init__(self, codigo: str, nome: str):
+        self.__codigo = codigo
         self.__nome = nome
 
     @property
     def nome(self):
         return self.__nome
     
+    @property
+    def codigo(self):
+        return self.__codigo
+    
     @nome.setter
     def nome(self, nome: str):
-        if not isinstance(nome, str):
+        if not isinstance(nome, str): # type: ignore
             raise TypeError("O nome do país deve ser uma string.")
         self.__nome = nome
+
+    
