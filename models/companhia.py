eaddec8e9d9d78c5861c4931e37e37852f6c4dc5
Eduardo Faleiro <eduardoviniciusfaleiro@gmail.com>
2025-10-03 00:59:20 -0300

    Alterado para remover erros pylance e adicionado copyWith

--- models/companhia.py
+++ models/companhia.py
@@ -1,19 +1,25 @@
+from typing import Optional
 from models.pais import Pais
 
 class Companhia:
-    def __init__(self, nome: str, pais_sede: Pais, navios: list, capitaes: list):
+    def __init__(self, id: int, nome: str, pais_sede: Pais, navios: list = [], capitaes: list = []):
+        self.__id = id
         self.__nome = nome
         self.__pais_sede = pais_sede
-        self.__navios = list(navios) if navios is not None else []
-        self.__capitaes = list(capitaes) if capitaes is not None else []
+        self.__navios = navios# type: ignore
+        self.__capitaes = capitaes# type: ignore
         
+    @property
+    def id(self):
+        return self.__id
+
     @property
     def nome(self):
         return self.__nome
 
     @nome.setter
     def nome(self, nome: str):
-        if not isinstance(nome, str):
+        if not isinstance(nome, str): # pyright: ignore[reportUnnecessaryIsInstance]
            raise TypeError("O nome da companhia deve ser uma string.")
         self.__nome = nome
         
@@ -23,14 +29,30 @@ class Companhia:
 
     @pais_sede.setter
     def pais_sede(self, pais_sede: Pais):
-        if not isinstance(pais_sede, Pais):
+        if not isinstance(pais_sede, Pais): # pyright: ignore[reportUnnecessaryIsInstance]
            raise TypeError("O nome do país sede deve ser uma instância da classe Pais.")
         self.__pais_sede = pais_sede
         
     @property
     def navios(self):
-        return self.__navios.copy()
+        return self.__navios.copy()# type: ignore
 
     @property
     def capitaes(self):
-        return self.__capitaes.copy()
+        return self.__capitaes.copy()# type: ignore
+    
+    def copyWith(
+        self,
+        id: Optional[int] = None,
+        nome: Optional[str] = None,
+        pais_sede: Optional[Pais] = None,
+        navios: Optional[list] = None, # type: ignore
+        capitaes: Optional[list] = None# type: ignore
+    ) -> 'Companhia':
+        return Companhia(
+            id=id if id is not None else self.__id,
+            nome=nome if nome is not None else self.__nome,
+            pais_sede=pais_sede if pais_sede is not None else self.__pais_sede,
+            navios=navios if navios is not None else self.__navios.copy(), # pyright: ignore[reportUnknownMemberType]
+            capitaes=capitaes if capitaes is not None else self.__capitaes.copy() # pyright: ignore[reportUnknownMemberType]
+        )
