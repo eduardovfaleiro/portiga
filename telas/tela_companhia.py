4bbcb88752c969a502a124966a2ef334f7b2a68f
Eduardo Faleiro <eduardoviniciusfaleiro@gmail.com>
2025-10-05 16:01:47 -0300

    refactor(tela_companhia.py): Lista de países agora são criadas na própria classe

--- telas/tela_companhia.py
+++ telas/tela_companhia.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import Any
 from models.companhia import Companhia
 from models.pais import Pais
@@ -8,6 +9,18 @@ import re
 
 class TelaCompanhia(TelaUtils):
     __opcoes = {1: 'Incluir', 2: 'Excluir', 3: 'Alterar', 4: 'Listar', 0: 'Retornar'}
+    __paises: dict[str, str] = {}
+        
+    PAISES_FILE_PATH = os.path.join(os.path.dirname(__file__), 'countries.json')
+    
+    try:
+        with open('countries.json', 'r', encoding='utf-8') as f:
+            paises_dict = json.load(f)
+            __paises = {item.get('code'): item.get('name') for item in paises_dict}
+    except FileNotFoundError:
+        print("ERRO: O arquivo countries.json não foi encontrado. A lista de países está vazia.")
+    except json.JSONDecodeError:
+        print("ERRO: Falha ao decodificar o JSON do arquivo countries.json.")
 
     def abre_opcoes(self):
         self.mostra_titulo('Companhias')
@@ -57,16 +70,8 @@ class TelaCompanhia(TelaUtils):
 
         return {"nome": nome, "pais_sede": pais}
     
-    # TODO(Corrigir)
     def retorna_pais(self, codigo_pais: str) -> Pais | None:
-        paises: dict[str, str] = {}
-        with open('countries.json', 'r', encoding='utf-8') as paises_json:
-            paises_dict = json.load(paises_json)
-
-            for pais_item in paises_dict:
-                paises[pais_item.get('code')] = pais_item.get('name')
-
-        pais_nome = paises.get(codigo_pais)
+        pais_nome = self.__paises.get(codigo_pais)
 
         if pais_nome is not None:
             return Pais(codigo=codigo_pais, nome=pais_nome)
