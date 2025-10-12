9479f281d948b4ab3e458d9f688cf66cd0cf5616
EduardoEAR <eduardoerichsen@gmail.com>
2025-10-12 13:36:05 -0300

    removido atributo "navio" da classe Capitao

--- models/capitao.py
+++ models/capitao.py
@@ -1,12 +1,10 @@
 from models.pessoa import Pessoa
 from models.companhia import Companhia
-from models.navio import Navio
 
 class Capitao(Pessoa):
-    def __init__(self, nome: str, companhia: Companhia, navio: Navio):
+    def __init__(self, nome: str, companhia: Companhia):
         super().__init__(nome)
         self.__companhia = companhia
-        self.__navio = navio
 
     @property
     def companhia(self):
@@ -17,13 +15,3 @@ class Capitao(Pessoa):
         if not isinstance(companhia, Companhia):
             raise TypeError("A companhia deve ser uma instância da classe Companhia.")
         self.__companhia = companhia
-
-    @property
-    def navio(self):
-        return self.__navio
-    
-    @navio.setter
-    def navio(self, navio: Navio):
-        if not isinstance(navio, Navio):
-            raise TypeError("O navio deve ser uma instância da classe Navio.")
-        self.__navio = navio
