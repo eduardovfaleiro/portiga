a9f3072b851cc25cbfc4f420ca343caf414f28f6
EduardoEAR <eduardoerichsen@gmail.com>
2025-10-12 13:36:36 -0300

    inclusão de nova opção no controlador e tela do sistema

--- controle/controlador_sistema.py
+++ controle/controlador_sistema.py
@@ -1,4 +1,5 @@
 from controle.controlador_companhia import ControladorCompanhia
+from controle.controlador_navio import ControladorNavio
 from telas.tela_sistema import TelaSistema
 
 
@@ -6,6 +7,7 @@ class ControladorSistema:
     def __init__(self):
         self.__tela_sistema = TelaSistema()
         self.__controlador_companhia = ControladorCompanhia(self)
+        self.__controlador_navio = ControladorNavio(self)
 
     def inicializa(self):
         self.abre_tela()
@@ -13,11 +15,14 @@ class ControladorSistema:
     def abre_tela_companhia(self):
         self.__controlador_companhia.abre_tela()
 
+    def abre_tela_navio(self):
+        self.__controlador_navio.abre_tela()
+
     def encerra(self):
         exit(0)
 
     def abre_tela(self):
-        opcoes = {1: self.abre_tela_companhia, 0: self.encerra}
+        opcoes = {2: self.abre_tela_companhia, 1: self.abre_tela_navio, 0: self.encerra}
 
         while True:
             opcao_escolhida = self.__tela_sistema.abre_opcoes()
