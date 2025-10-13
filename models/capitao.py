c1aaceadd49fcca05893f9b9a36d5e3e4bc42ba3
Eduardo Faleiro <eduardoviniciusfaleiro@gmail.com>
2025-10-13 01:34:56 -0300

    Corrigido Capitao que não usava super

--- models/capitao.py
+++ models/capitao.py
@@ -3,5 +3,3 @@ from models.pessoa import Pessoa
 class Capitao(Pessoa):
     def __init__(self, id: int, nome: str):
         super().__init__(id, nome)
-        self.__id = id
-        self.__nome = nome
