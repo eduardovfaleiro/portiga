408e82f82ee6639d4d82159d1b0504b215841ee7
EduardoEAR <eduardoerichsen@gmail.com>
2025-11-23 19:13:33 -0300

    Criação das classes DAO chegada e partida

---
+++ DAOs/chegada_dao.py
@@ -0,0 +1,22 @@
+from DAOs.dao import DAO
+from models.chegada import Chegada
+
+class ChegadaDAO(DAO):
+    def __init__(self):
+        super().__init__('chegadas.pkl')
+
+    def add(self, chegada: Chegada):
+        if((chegada is not None) and isinstance(chegada, Chegada) and isinstance(chegada.id, int)):
+            super().add(chegada.id, chegada)
+
+    def update(self, chegada: Chegada):
+        if((chegada is not None) and isinstance(chegada, Chegada) and isinstance(chegada.id, int)):
+            super().update(chegada.id, chegada)
+
+    def get(self, key:int):
+        if isinstance(key, int):
+            return super().get(key)
+
+    def remove(self, key:int):
+        if(isinstance(key, int)):
+            return super().remove(key)
---
+++ DAOs/partida_dao.py
@@ -0,0 +1,22 @@
+from DAOs.dao import DAO
+from models.partida import Partida
+
+class PartidaDAO(DAO):
+    def __init__(self):
+        super().__init__('partidas.pkl')
+
+    def add(self, partida: Partida):
+        if((partida is not None) and isinstance(partida, Partida) and isinstance(partida.id, int)):
+            super().add(partida.id, partida)
+
+    def update(self, partida: Partida):
+        if((partida is not None) and isinstance(partida, Partida) and isinstance(partida.id, int)):
+            super().update(partida.id, partida)
+
+    def get(self, key:int):
+        if isinstance(key, int):
+            return super().get(key)
+
+    def remove(self, key:int):
+        if(isinstance(key, int)):
+            return super().remove(key)
